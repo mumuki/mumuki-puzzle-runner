59c8caa06143393bcea2e3eed8c2b58d38aa4034
Franco Bulgarelli <franco@mumuki.org>
2020-07-12 17:10:56 -0300

    Allowing simple tests at server side

--- lib/test_hook.rb
+++ lib/test_hook.rb
@@ -1,13 +1,26 @@
 class PuzzleTestHook < Mumukit::Templates::FileHook
-  def run!(request)
-    ['', :passed]
+  def run!(req)
+    points = parse_points req
+    if !points || points_relatively_equal(points, JSON.parse(req.content))
+      ['', :passed]
+    else
+      ['', :failed]
+    end
   end
 
   def compile(req)
-    puts req
     req
   end
 
+  def parse_points(req)
+    (req.test =~ /Muzzle\.expect\((.*)\)/) && (JSON.parse($1) rescue nil)
+  end
+
+  def points_relatively_equal(expected, actual)
+    x_offset = actual.first.first - (100 * expected.first.first)
+    y_offset = actual.first[1]  - (100 * expected.first[1])
+    actual.flat_map { |x, y| [x - x_offset, y - y_offset] } == expected.flatten.map { |it| it * 100 }
+  end
 end
 
 
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -10,17 +10,77 @@ describe 'runner' do
   end
   after(:all) { Process.kill 'TERM', @pid }
 
-  it 'sample test' do
-    response = bridge.run_tests!(test: '...',
-                                 extra: '...',
-                                 content: '...',
+  it 'right exact positions' do
+    response = bridge.run_tests!(test: "Muzzle.expect([[1, 2], [1, 3]])\nMuzzle.basic(1, 2, 'an_image.png')\n",
+                                 extra: '',
+                                 content: '[[100, 200], [100, 300]]',
                                  expectations: [])
 
-    expect(response).to eq(response_type: :structured,
+    expect(response).to eq(response_type: :unstructured,
                            test_results: [],
                            status: :passed,
                            feedback: '',
                            expectation_results: [],
                            result: '')
   end
+
+  it 'wrong exact positions' do
+    response = bridge.run_tests!(test: "Muzzle.expect([[1, 2], [1, 3]])\nMuzzle.basic(1, 2, 'an_image.png')\n",
+                                 extra: '',
+                                 content: '[[100, 200], [100, 400]]',
+                                 expectations: [])
+
+    expect(response).to eq(response_type: :unstructured,
+                           test_results: [],
+                           status: :failed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: '')
+  end
+
+
+  it 'right relative positions' do
+    response = bridge.run_tests!(test: "Muzzle.expect([[1, 2], [1, 3]])\nMuzzle.basic(1, 2, 'an_image.png')\n",
+                                 extra: '',
+                                 content: '[[200, 300], [200, 400]]',
+                                 expectations: [])
+
+    expect(response).to eq(response_type: :unstructured,
+                           test_results: [],
+                           status: :passed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: '')
+  end
+
+  it 'wrong relative positions' do
+    response = bridge.run_tests!(test: "Muzzle.expect([[1, 2], [1, 3]])\nMuzzle.basic(1, 2, 'an_image.png')\n",
+                                 extra: '',
+                                 content: '[[200, 300], [200, 450]]',
+                                 expectations: [])
+
+    expect(response).to eq(response_type: :unstructured,
+                           test_results: [],
+                           status: :failed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: '')
+  end
+
+
+
+  it 'no positions' do
+    response = bridge.run_tests!(test: "Muzzle.basic(1, 2, 'an_image.png')\n",
+                                 extra: '',
+                                 content: '[[200, 300], [200, 400]]',
+                                 expectations: [])
+
+    expect(response).to eq(response_type: :unstructured,
+                           test_results: [],
+                           status: :passed,
+                           feedback: '',
+                           expectation_results: [],
+                           result: '')
+  end
+
 end
